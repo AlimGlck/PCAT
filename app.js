86b92a1f63f04794c221605280a6fa29cfcc3452
Alim Gölcük <alimglck5421@gmail.com>
2022-11-23 22:57:33 +0300

    inner page added

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
+const path = require('path');
+
+const Photo = require('./models/Photo');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+  
 });
 
 app.get('/add', (req, res) => {
@@ -18,6 +30,18 @@ app.get('/add', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+app.get('/photos/:id', async (req,res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo',{
+    photo,
+  });
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
